36e6d0acf74469f1e3fcdf893dc17d69b796ed6b
Leprekus <leprekus@yahoo.com>
2023-05-04 23:18:15 -0700

    revert name

--- lib/Options.ts
+++ lib/Options.ts
@@ -1,5 +1,5 @@
 const options = {
-    baseUrl: process.env.NEXTAUTH_URL ||'http://localhost:3000/' 
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL ||'http://localhost:3000/' 
 }
 
 export default options
