1db1067deac0e38d8cd1e69fa33744763ce7b662
Leprekus <leprekus@yahoo.com>
2023-05-06 19:43:37 -0700

    create api route

---
+++ pages/api/post/comments.ts
@@ -0,0 +1,25 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { name, id } = JSON.parse(req.body)
+  //token = Bearer token
+  var token = req.headers.authorization 
+ 
+  const params = new URLSearchParams({ id: name, dir})
+  const url = 'https://oauth.reddit.com/api/vote?' + params
+  
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'authorization': (req.headers.authorization as string),
+      'content-type': 'application/x-www-form-urlencoded;charset=UTF-8'  
+    },
+  })
+
+  if(!response.ok) {
+    return res.status(response.status).json({ message: 'Reddit: ' + response.statusText })
+  }
+  return res.status(200).json({ message: response.statusText })
+  
+  }
+
+export default handler
