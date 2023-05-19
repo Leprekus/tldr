58f9a1bc684f5d77e048efe9a2e0e7559a22d0f4
Leprekus <leprekus@yahoo.com>
2023-05-19 12:33:32 -0700

    create error component and route

---
+++ app/components/Error.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Error() {
+  return (
+    <div role="alert">
+    <p>Something went wrong:</p>
+    <pre style={{ color: "red" }}>asdv </pre>
+  </div>
+  )
+}
