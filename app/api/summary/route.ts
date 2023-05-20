eca80c611a67c234f58e0e7fcb239bd6cd7c3215
Leprekus <leprekus@yahoo.com>
2023-05-20 13:21:43 -0700

    remove summarizer

--- app/api/summary/route.ts
+++ app/api/summary/route.ts
@@ -1,14 +1,10 @@
 import authenticateClient from '@/utils/authenticateClient';
 import { NextResponse } from 'next/server';
-import RedditWrapper from '@/lib/RedditWrapper';
-const SummarizerManager = require("node-summarizer").SummarizerManager;
 
 
 export async function POST(req: Request) {
     const text = await req.json()
-    const summarizer = new SummarizerManager(text, 3)
-    const summary = summarizer.getSummaryByFrequency().summary
-    return NextResponse.json({ summary })
+    return NextResponse.json({ summary: text })
 
 }
 
